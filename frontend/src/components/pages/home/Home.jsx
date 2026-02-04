7285d62090fc9ff6478c3f28845a3b3648699337
Ugnytea <ugnia.stankeviciute@gmail.com>
2026-02-04 23:00:52 +0200

    Make main task tab

--- frontend/src/components/pages/home/Home.jsx
+++ frontend/src/components/pages/home/Home.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
-import Sidebar from "../../common/sidebar";
+
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="task-container">
-      <div className="heading">
+      <div className="header">
         <h2>Tasks</h2>
-        <div className="tags">
-          <h3>Due date</h3>
-          <h3>Lists</h3>
-        </div>
+        <h3>Due date</h3>
+        <h3>Lists</h3>
       </div>
 
       <div className="task-box">
-        {/* <h3>Task one</h3> */}
         <section className="task">
           <span className="dot"></span>
           <h3>Task one</h3>
+          <h3 className="tags">2026-02-10</h3>
+          <h3 className="tags">List one</h3>
+          <img src="/public/icons/star-filled.png" />
+        </section>
+        <section className="task">
+          <span className="dot"></span>
+          <h3>Task one</h3>
+          <h3 className="tags"></h3>
+          <h3 className="tags">List two</h3>
+          <img src="/public/icons/star-filled.png" />
+        </section>
+        <section className="task">
+          <span className="dot"></span>
+          <h3>Task one</h3>
+          <h3 className="tags">2026-02-10</h3>
+          <h3 className="tags"></h3>
+          <img src="/public/icons/star-not-filled.png" />
         </section>
       </div>
     </div>
